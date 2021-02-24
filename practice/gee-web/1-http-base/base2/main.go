4eb5251559067c4eed4d41ce21469dd709115217
zander-Y9000X <zander@zuru.tech>
2021-02-24 17:42:20 +0800

    Finished Day1 practice for gee-web

---
+++ practice/gee-web/1-http-base/base2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Engine struct{}
+
+// 这里将结构体指针绑定了函数，实现函数赋值变量
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":7999", engine))
+}
