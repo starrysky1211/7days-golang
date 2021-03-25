9ab76fb9f801d4d2451270e0c4ea3aee3e7d51fa
zander-Y9000X <zander@zuru.tech>
2021-03-25 11:18:20 +0800

    finished template

---
+++ practice/gee-web/6-template/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"gee"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+// template area
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+// middleware area
+func Logger() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// Start timer
+		t := time.Now()
+		// Process request
+		c.Next()
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		t := time.Now()
+		c.Fail(500, "Internal Server Error")
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+// main area
+
+func main() {
+	r := gee.New()
+	r.Use(Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+	stu1 := &student{Name: "Zander", Age: 28}
+	stu2 := &student{Name: "Sim", Age: 27}
+
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+
+	r.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "css.tmpl", nil)
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	r.GET("/date", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Date(2021, 3, 24, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello, %s, you are at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.POST("/login", func(c *gee.Context) {
+			c.Json(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+
+	r.Run(":7999")
+}
